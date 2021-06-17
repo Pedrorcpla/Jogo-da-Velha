d381511268543ac07b7a17e2c22f7d5719d66bc1
Pedrorcpla <pedrorcpla@gmail.com>
2021-06-17 14:24:14 -0300

    Jogo Funcional

---
+++ Form1.Designer.cs
@@ -0,0 +1,363 @@
+﻿
+namespace Jogo_da_Velha
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.opcaoA1 = new System.Windows.Forms.CheckBox();
+            this.opcaoA2 = new System.Windows.Forms.CheckBox();
+            this.opcaoA3 = new System.Windows.Forms.CheckBox();
+            this.opcaoB3 = new System.Windows.Forms.CheckBox();
+            this.opcaoB2 = new System.Windows.Forms.CheckBox();
+            this.opcaoB1 = new System.Windows.Forms.CheckBox();
+            this.opcaoC3 = new System.Windows.Forms.CheckBox();
+            this.opcaoC2 = new System.Windows.Forms.CheckBox();
+            this.opcaoC1 = new System.Windows.Forms.CheckBox();
+            this.a1 = new System.Windows.Forms.Label();
+            this.a2 = new System.Windows.Forms.Label();
+            this.a3 = new System.Windows.Forms.Label();
+            this.b3 = new System.Windows.Forms.Label();
+            this.b2 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Label();
+            this.c3 = new System.Windows.Forms.Label();
+            this.c2 = new System.Windows.Forms.Label();
+            this.c1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.sair = new System.Windows.Forms.Button();
+            this.resultado = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // opcaoA1
+            // 
+            this.opcaoA1.AutoSize = true;
+            this.opcaoA1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoA1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opcaoA1.Location = new System.Drawing.Point(324, 188);
+            this.opcaoA1.Name = "opcaoA1";
+            this.opcaoA1.Size = new System.Drawing.Size(15, 14);
+            this.opcaoA1.TabIndex = 0;
+            this.opcaoA1.UseVisualStyleBackColor = true;
+            this.opcaoA1.CheckedChanged += new System.EventHandler(this.opcaoA1_CheckedChanged);
+            // 
+            // opcaoA2
+            // 
+            this.opcaoA2.AutoSize = true;
+            this.opcaoA2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoA2.Location = new System.Drawing.Point(397, 188);
+            this.opcaoA2.Name = "opcaoA2";
+            this.opcaoA2.Size = new System.Drawing.Size(15, 14);
+            this.opcaoA2.TabIndex = 1;
+            this.opcaoA2.UseVisualStyleBackColor = true;
+            this.opcaoA2.CheckedChanged += new System.EventHandler(this.opcaoA2_CheckedChanged);
+            // 
+            // opcaoA3
+            // 
+            this.opcaoA3.AutoSize = true;
+            this.opcaoA3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoA3.Location = new System.Drawing.Point(468, 188);
+            this.opcaoA3.Name = "opcaoA3";
+            this.opcaoA3.Size = new System.Drawing.Size(15, 14);
+            this.opcaoA3.TabIndex = 2;
+            this.opcaoA3.UseVisualStyleBackColor = true;
+            this.opcaoA3.CheckedChanged += new System.EventHandler(this.opcaoA3_CheckedChanged);
+            // 
+            // opcaoB3
+            // 
+            this.opcaoB3.AutoSize = true;
+            this.opcaoB3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoB3.Location = new System.Drawing.Point(468, 239);
+            this.opcaoB3.Name = "opcaoB3";
+            this.opcaoB3.Size = new System.Drawing.Size(15, 14);
+            this.opcaoB3.TabIndex = 5;
+            this.opcaoB3.UseVisualStyleBackColor = true;
+            this.opcaoB3.CheckedChanged += new System.EventHandler(this.opcaoB3_CheckedChanged);
+            // 
+            // opcaoB2
+            // 
+            this.opcaoB2.AutoSize = true;
+            this.opcaoB2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoB2.Location = new System.Drawing.Point(397, 239);
+            this.opcaoB2.Name = "opcaoB2";
+            this.opcaoB2.Size = new System.Drawing.Size(15, 14);
+            this.opcaoB2.TabIndex = 4;
+            this.opcaoB2.UseVisualStyleBackColor = true;
+            this.opcaoB2.CheckedChanged += new System.EventHandler(this.opcaoB2_CheckedChanged);
+            // 
+            // opcaoB1
+            // 
+            this.opcaoB1.AutoSize = true;
+            this.opcaoB1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoB1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opcaoB1.Location = new System.Drawing.Point(324, 239);
+            this.opcaoB1.Name = "opcaoB1";
+            this.opcaoB1.Size = new System.Drawing.Size(15, 14);
+            this.opcaoB1.TabIndex = 3;
+            this.opcaoB1.UseVisualStyleBackColor = true;
+            this.opcaoB1.CheckedChanged += new System.EventHandler(this.opcaoB1_CheckedChanged);
+            // 
+            // opcaoC3
+            // 
+            this.opcaoC3.AutoSize = true;
+            this.opcaoC3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoC3.Location = new System.Drawing.Point(468, 298);
+            this.opcaoC3.Name = "opcaoC3";
+            this.opcaoC3.Size = new System.Drawing.Size(15, 14);
+            this.opcaoC3.TabIndex = 8;
+            this.opcaoC3.UseVisualStyleBackColor = true;
+            this.opcaoC3.CheckedChanged += new System.EventHandler(this.opcaoC3_CheckedChanged);
+            // 
+            // opcaoC2
+            // 
+            this.opcaoC2.AutoSize = true;
+            this.opcaoC2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoC2.Location = new System.Drawing.Point(397, 298);
+            this.opcaoC2.Name = "opcaoC2";
+            this.opcaoC2.Size = new System.Drawing.Size(15, 14);
+            this.opcaoC2.TabIndex = 7;
+            this.opcaoC2.UseVisualStyleBackColor = true;
+            this.opcaoC2.CheckedChanged += new System.EventHandler(this.opcaoC2_CheckedChanged);
+            // 
+            // opcaoC1
+            // 
+            this.opcaoC1.AutoSize = true;
+            this.opcaoC1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.opcaoC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.opcaoC1.Location = new System.Drawing.Point(324, 298);
+            this.opcaoC1.Name = "opcaoC1";
+            this.opcaoC1.Size = new System.Drawing.Size(15, 14);
+            this.opcaoC1.TabIndex = 6;
+            this.opcaoC1.UseVisualStyleBackColor = true;
+            this.opcaoC1.CheckedChanged += new System.EventHandler(this.opcaoC1_CheckedChanged);
+            // 
+            // a1
+            // 
+            this.a1.AutoSize = true;
+            this.a1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a1.Location = new System.Drawing.Point(319, 181);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(0, 25);
+            this.a1.TabIndex = 9;
+            // 
+            // a2
+            // 
+            this.a2.AutoSize = true;
+            this.a2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a2.Location = new System.Drawing.Point(392, 181);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(0, 25);
+            this.a2.TabIndex = 10;
+            // 
+            // a3
+            // 
+            this.a3.AutoSize = true;
+            this.a3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.a3.Location = new System.Drawing.Point(463, 181);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(0, 25);
+            this.a3.TabIndex = 11;
+            // 
+            // b3
+            // 
+            this.b3.AutoSize = true;
+            this.b3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b3.Location = new System.Drawing.Point(463, 232);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(0, 25);
+            this.b3.TabIndex = 14;
+            // 
+            // b2
+            // 
+            this.b2.AutoSize = true;
+            this.b2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b2.Location = new System.Drawing.Point(392, 232);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(0, 25);
+            this.b2.TabIndex = 13;
+            // 
+            // b1
+            // 
+            this.b1.AutoSize = true;
+            this.b1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b1.Location = new System.Drawing.Point(319, 232);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(0, 25);
+            this.b1.TabIndex = 12;
+            // 
+            // c3
+            // 
+            this.c3.AutoSize = true;
+            this.c3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c3.Location = new System.Drawing.Point(463, 289);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(0, 25);
+            this.c3.TabIndex = 17;
+            // 
+            // c2
+            // 
+            this.c2.AutoSize = true;
+            this.c2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c2.Location = new System.Drawing.Point(392, 289);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(0, 25);
+            this.c2.TabIndex = 16;
+            // 
+            // c1
+            // 
+            this.c1.AutoSize = true;
+            this.c1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.c1.Location = new System.Drawing.Point(319, 289);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(0, 25);
+            this.c1.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.label1.Location = new System.Drawing.Point(232, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(346, 59);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "JOGO DA VELHA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(203, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(388, 23);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "O primeiro jogador será o X,  o segundo será o O.";
+            // 
+            // sair
+            // 
+            this.sair.BackColor = System.Drawing.Color.DodgerBlue;
+            this.sair.FlatAppearance.BorderSize = 0;
+            this.sair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.sair.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.sair.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.sair.Location = new System.Drawing.Point(658, 379);
+            this.sair.Name = "sair";
+            this.sair.Size = new System.Drawing.Size(130, 32);
+            this.sair.TabIndex = 20;
+            this.sair.Text = "Sair";
+            this.sair.UseVisualStyleBackColor = false;
+            this.sair.Click += new System.EventHandler(this.sair_Click);
+            // 
+            // resultado
+            // 
+            this.resultado.AutoSize = true;
+            this.resultado.Font = new System.Drawing.Font("Calibri", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultado.Location = new System.Drawing.Point(307, 117);
+            this.resultado.Name = "resultado";
+            this.resultado.Size = new System.Drawing.Size(0, 29);
+            this.resultado.TabIndex = 21;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DodgerBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(522, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 32);
+            this.button1.TabIndex = 22;
+            this.button1.Text = "Recomeçar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(800, 423);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.resultado);
+            this.Controls.Add(this.sair);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.c3);
+            this.Controls.Add(this.c2);
+            this.Controls.Add(this.c1);
+            this.Controls.Add(this.b3);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a3);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.opcaoC3);
+            this.Controls.Add(this.opcaoC2);
+            this.Controls.Add(this.opcaoC1);
+            this.Controls.Add(this.opcaoB3);
+            this.Controls.Add(this.opcaoB2);
+            this.Controls.Add(this.opcaoB1);
+            this.Controls.Add(this.opcaoA3);
+            this.Controls.Add(this.opcaoA2);
+            this.Controls.Add(this.opcaoA1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox opcaoA1;
+        private System.Windows.Forms.CheckBox opcaoA2;
+        private System.Windows.Forms.CheckBox opcaoA3;
+        private System.Windows.Forms.CheckBox opcaoB3;
+        private System.Windows.Forms.CheckBox opcaoB2;
+        private System.Windows.Forms.CheckBox opcaoB1;
+        private System.Windows.Forms.CheckBox opcaoC3;
+        private System.Windows.Forms.CheckBox opcaoC2;
+        private System.Windows.Forms.CheckBox opcaoC1;
+        private System.Windows.Forms.Label a1;
+        private System.Windows.Forms.Label a2;
+        private System.Windows.Forms.Label a3;
+        private System.Windows.Forms.Label b3;
+        private System.Windows.Forms.Label b2;
+        private System.Windows.Forms.Label b1;
+        private System.Windows.Forms.Label c3;
+        private System.Windows.Forms.Label c2;
+        private System.Windows.Forms.Label c1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button sair;
+        private System.Windows.Forms.Label resultado;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Jogo_da_Velha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int jogador = 0;
+
+        private void sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void opcaoA1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(jogador%2 == 0)
+            {
+                a1.Text = "X";
+            }
+            else
+            {
+                a1.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoA2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador%2 == 0)
+            {
+                a2.Text = "X";
+            }
+            else
+            {
+                a2.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoA3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador % 2 == 0)
+            {
+                a3.Text = "X";
+            }
+            else
+            {
+                a3.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoB1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador % 2 == 0)
+            {
+                b1.Text = "X";
+            }
+            else
+            {
+                b1.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoB2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador % 2 == 0)
+            {
+                b2.Text = "X";
+            }
+            else
+            {
+                b2.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoB3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador % 2 == 0)
+            {
+                b3.Text = "X";
+            }
+            else
+            {
+                b3.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoC1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador % 2 == 0)
+            {
+                c1.Text = "X";
+            }
+            else
+            {
+                c1.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoC2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador % 2 == 0)
+            {
+                c2.Text = "X";
+            }
+            else
+            {
+                c2.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+
+        private void opcaoC3_CheckedChanged(object sender, EventArgs e)
+        {
+            if (jogador % 2 == 0)
+            {
+                c3.Text = "X";
+            }
+            else
+            {
+                c3.Text = "O";
+            }
+            verificarVencedor();
+            jogador++;
+        }
+        public void verificarVencedor()
+        {
+            if ((a1.Text == "X" && a2.Text == "X" && a3.Text == "X") || (b1.Text == "X" && b2.Text == "X" && b3.Text == "X") || (c1.Text == "X" && c2.Text == "X" && c3.Text == "X") || (a1.Text == "X" && b2.Text == "X" && c3.Text == "X") || (a3.Text == "X" && b2.Text == "X" && c1.Text == "X") || (a1.Text == "X" && b1.Text == "X" && c1.Text == "X") || (a2.Text == "X" && b2.Text == "X" && c2.Text == "X") || (a3.Text == "X" && b3.Text == "X" && c3.Text == "X"))
+            {
+                resultado.Text = "Jogador 1 Venceu!";
+            }
+            else if ((a1.Text == "O" && a2.Text == "O" && a3.Text == "O") || (b1.Text == "O" && b2.Text == "O" && b3.Text == "O") || (c1.Text == "O" && c2.Text == "O" && c3.Text == "O") || (a1.Text == "O" && b2.Text == "O" && c3.Text == "O") || (a3.Text == "O" && b2.Text == "O" && c1.Text == "O") || (a1.Text == "O" && b1.Text == "O" && c1.Text == "O") || (a2.Text == "O" && b2.Text == "O" && c2.Text == "O") || (a3.Text == "O" && b3.Text == "O" && c3.Text == "O"))
+            {
+                resultado.Text = "Jogador 2 Venceu!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            jogador = -1;
+
+            opcaoA1.Checked = false;
+            a1.Text = null;
+            opcaoA2.Checked = false;
+            a2.Text = null;
+            opcaoA3.Checked = false;
+            a3.Text = null;
+
+            opcaoB1.Checked = false;
+            b1.Text = null;
+            opcaoB2.Checked = false;
+            b2.Text = null;
+            opcaoB3.Checked = false;
+            b3.Text = null;
+
+            opcaoC1.Checked = false;
+            c1.Text = null;
+            opcaoC2.Checked = false;
+            c2.Text = null;
+            opcaoC3.Checked = false;
+            c3.Text = null;
+
+            resultado.Text = "";
+        }
+    }
+}
